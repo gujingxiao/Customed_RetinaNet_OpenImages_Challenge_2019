121e4906fca250cbee31e333fd20a06138499d40
seigato <gujingxiao@hotmail.com>
2019-07-03 15:18:30 +0800

    Add files via upload
    新增level1和level5的label筛选脚本，可以直接生成筛选后的训练csv

---
+++ labelProcess/tooMuch_label_select.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import numpy as np
+
+# Level 1
+# 'Man', 'Tree', 'Human face', 'Person', 'Woman', 'Footwear', 'Window'
+TOO_MUCH_LABELS = ['/m/04yx4', '/m/07j7r', '/m/0dzct', '/m/01g317', '/m/03bt1vf', '/m/09j5n', '/m/0d4v4']
+csvfile = '/media/gujingxiao/f577505e-73a2-41d0-829c-eb4d01efa827/OpenimageV5/output/level_1_files/train-exists-annotations-bbox-level-1.csv'
+savefile = '/media/gujingxiao/f577505e-73a2-41d0-829c-eb4d01efa827/OpenimageV5/output/level_1_files/train-exists-annotations-bbox-level-1-select.csv'
+
+# Level 5
+# TOO_MUCH_LABELS = ['/m/03q69', '/m/0dzf4', '/m/04hgtk']
+# csvfile = '/media/gujingxiao/f577505e-73a2-41d0-829c-eb4d01efa827/OpenimageV5/output/level_5_files/train-exists-annotations-bbox-level-5.csv'
+# savefile = '/media/gujingxiao/f577505e-73a2-41d0-829c-eb4d01efa827/OpenimageV5/output/level_5_files/train-exists-annotations-bbox-level-5-select.csv'
+
+boxes = pd.read_csv(csvfile)
+idList = boxes['ImageID']
+labelList = boxes['LabelName']
+
+print('Analysing tooMuch Infos...')
+tooMuch_boxes = boxes[boxes['LabelName'].isin(TOO_MUCH_LABELS)]
+tooMuch_index_sort = tooMuch_boxes['ImageID'].value_counts().sort_index()
+
+print('Analysing tooMuch Ori Infos...')
+tooMuch_ori_boxes = boxes[boxes['ImageID'].isin(tooMuch_boxes['ImageID'].value_counts().index)]
+tooMuch_ori_index_sort = tooMuch_ori_boxes['ImageID'].value_counts().sort_index()
+
+bboxList = []
+for idx in range(len(tooMuch_ori_index_sort)):
+    tooMuch_name = tooMuch_index_sort.index[idx]
+    tooMuch_number = tooMuch_index_sort.values[idx]
+    tooMuch_ori_name = tooMuch_ori_index_sort.index[idx]
+    tooMuch_ori_number = tooMuch_ori_index_sort.values[idx]
+
+    if float(tooMuch_number) / float(tooMuch_ori_number) > 0.75:
+        if idx % 10000 == 0:
+            print(idx, ' / ', len(tooMuch_ori_index_sort))
+        bboxList.append(tooMuch_name)
+
+reduced_boxes = boxes[~boxes['ImageID'].isin(bboxList)]
+print(boxes['LabelName'].value_counts())
+print(reduced_boxes['LabelName'].value_counts())
+print(len(reduced_boxes['ImageID'].value_counts().index), len(reduced_boxes['LabelName']))
+reduced_boxes.to_csv(savefile, index=False)
